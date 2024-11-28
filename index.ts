1029f1ca7bb475044bd114544aecd0c5871eb3f9
Lee Salminen <leesalminen@gmail.com>
2024-11-28 13:56:42 -0600

    adsf

--- index.ts
+++ index.ts
@@ -1,4 +1,1 @@
-import levenshtein from 'fast-levenshtein';
 export { areNamesSimilar } from './lib/index.js';
-export { levenshtein };
-export type { LevenshteinOptions } from './lib/types/fast-levenshtein';
--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,1 +1,11 @@
- 
+export interface LevenshteinOptions {
+    useCollator?: boolean;
+    replacementCost?: number;
+}
+
+declare module 'fast-levenshtein' {
+    const levenshtein: {
+        get(str1: string, str2: string, options?: LevenshteinOptions): number;
+    };
+    export default levenshtein;
+} 
