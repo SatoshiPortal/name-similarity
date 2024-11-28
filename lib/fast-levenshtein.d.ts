962d06537e359be5747e9b4596958dc566506c66
Lee Salminen <leesalminen@gmail.com>
2024-11-28 13:41:21 -0600

    try it here

---
+++ lib/fast-levenshtein.d.ts
@@ -0,0 +1,12 @@
+declare module 'fast-levenshtein' {
+    interface LevenshteinOptions {
+        useCollator?: boolean;
+        replacementCost?: number;
+    }
+
+    const levenshtein: {
+        get(str1: string, str2: string, options?: LevenshteinOptions): number;
+    };
+
+    export default levenshtein;
+} 
--- lib/types.ts
+++
@@ -1,8 +0,0 @@
-export interface LevenshteinOptions {
-    useCollator?: boolean;
-    replacementCost?: number;
-}
-
-export interface Levenshtein {
-    get(str1: string, str2: string, options?: LevenshteinOptions): number;
-} 
